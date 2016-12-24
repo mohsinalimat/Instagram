a46eeea399a2514e3458c075b4c7e2a77f4ba1e2
cruisediary <cruzdiary@gmail.com>
2016-12-24 22:07:22 +0900

    feat: Add SignUp request model

---
+++ Instagram/Models/SignUp.swift
@@ -0,0 +1,21 @@
+//
+//  SignUp.swift
+//  Instagram
+//
+//  Created by CruzDiary on 24/12/2016.
+//  Copyright © 2016 cruz. All rights reserved.
+//
+
+import UIKit
+
+struct SignUp {
+    struct Request {
+        var userName: String
+        var email: String
+        var password: String
+    }
+    
+    struct Response {
+        
+    }
+}

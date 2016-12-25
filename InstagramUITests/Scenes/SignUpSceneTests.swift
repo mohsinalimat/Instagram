1a76e23e8a130d2b57150e5d41d8f0a04d8b5d78
cruisediary <cruzdiary@gmail.com>
2016-12-25 16:42:43 +0900

    test(uitest): Add signup scene UITest case
    email, userName, password textfield should be exists

---
+++ InstagramUITests/Scenes/SignUpSceneTests.swift
@@ -0,0 +1,15 @@
+//
+//  SignUpSceneTests.swift
+//  Instagram
+//
+//  Created by CruzDiary on 25/12/2016.
+//  Copyright © 2016 cruz. All rights reserved.
+//
+
+import XCTest
+
+class SignUpSceneTests: UITestCase {
+    func testUIElement() {
+        XCTAssertEqual(app.textFields.count, 3, "email, userName, password")
+    }
+}

af51293a85548999e634a3cf410a57fc40b69a5f
cruisediary <cruzdiary@gmail.com>
2017-01-05 23:43:51 +0900

    feat: SignUpViewController connect IBOutlet and drive to presentButtonViewModel

--- Instagram/Scenes/SignUp/SignUpViewController.swift
+++ Instagram/Scenes/SignUp/SignUpViewController.swift
@@ -8,11 +8,46 @@
 
 import UIKit
 
+import SkyFloatingLabelTextField
+import RxCocoa
+import RxSwift
+
+protocol SignUpViewControllerOutput {
+    var signUpButtonDidTap: PublishSubject<Void> { get }
+    
+    var presentButtonViewModel: Driver<SignUpScene.ButtonViewModel> { get }
+}
+
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: SkyFloatingLabelTextField!
+    @IBOutlet weak var userNameTextField: SkyFloatingLabelTextField!
+    @IBOutlet weak var passwordTextField: SkyFloatingLabelTextField!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    var viewModel: SignUpViewControllerOutput!
+    
+    let disposeBag = DisposeBag()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.viewModel = SignUpViewModel()
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        emailTextField.selectedTitleColor = .black
+        userNameTextField.selectedTitleColor = .black
+        passwordTextField.selectedTitleColor = .black
+        
+        viewModel.presentButtonViewModel
+            .drive(onNext: { [weak self] viewModel in
+            guard let `self` = self else { return }
+            self.signUpButton.backgroundColor = viewModel.backgroundColor
+        }).addDisposableTo(disposeBag)
         // Do any additional setup after loading the view.
     }
 

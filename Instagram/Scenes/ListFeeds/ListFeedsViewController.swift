0718ec20fb1a6290662dbcf057f2cb6364ec8e28
cruisediary <cruzdiary@gmail.com>
2017-01-07 19:28:06 +0900

    Feat: Add ListFeeds Scene

---
+++ Instagram/Scenes/ListFeeds/ListFeedsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ListFeedsViewController.swift
+//  Instagram
+//
+//  Created by CruzDiary on 07/01/2017.
+//  Copyright © 2017 cruz. All rights reserved.
+//
+
+import UIKit
+
+class ListFeedsViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
